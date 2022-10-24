6125a10c934d3fb52e678a80d9f417a0d089b2f6
seho <seho27060@gmail.com>
2022-10-25 00:35:30 +0900

    CH.6 구글 로그인 준비

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -29,6 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm");
     }
 }
